d0397d4e0efda244db2dba07b4a1062b78c83126
Deniz Yildirim <deyi@itu.dk>
2021-09-07 01:20:31 +0200

    Implemented error handling.

--- Lecture01/Program.cs
+++ Lecture01/Program.cs
@@ -9,7 +9,20 @@ namespace Lecture01
             Console.WriteLine("Please write the year you wish to check is a leap-year");
             
             var input = Console.ReadLine();
-            var year = Convert.ToInt32(input);
+            int year;
+
+            try {
+                year = Convert.ToInt32(input);
+
+                if (year < 1582) {
+                    Console.WriteLine("You must use a year that is more than 1582");
+                    return;
+                }
+            }
+            catch (Exception e) {
+                Console.WriteLine("You did not enter a number");
+                return;
+            }
 
             Console.WriteLine(new YearManager().IsLeapYear(year));
         }
