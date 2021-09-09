8683623f8bf96830c3b791637aec8af9657a9957
DenizYil <74733800+DenizYil@users.noreply.github.com>
2021-09-09 14:41:51 +0200

    Add everything regarding leap year (#4)
    Unideal merge

--- Lecture01/Program.cs
+++ Lecture01/Program.cs
@@ -6,7 +6,25 @@ namespace Lecture01
     {
         public static void Main(string[] args)
         {
+            Console.WriteLine("Please write the year you wish to check is a leap-year");
             
+            var input = Console.ReadLine();
+            int year;
+
+            try {
+                year = Convert.ToInt32(input);
+
+                if (year < 1582) {
+                    Console.WriteLine("You must use a year that is more than 1582");
+                    return;
+                }
+            }
+            catch (Exception ignored) {
+                Console.WriteLine("You did not enter a number");
+                return;
+            }
+
+            Console.WriteLine(new YearManager().IsLeapYear(year) ? "yay" : "nay");
         }
     }
 }
